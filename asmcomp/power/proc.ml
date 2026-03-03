4ad0986ec8f4af9728381c88743124d6b7ce6a9f
Mark Elvers <mark.elvers@tunbury.org>
2026-03-03 14:12:18 +0000

    ppc32 backend

--- asmcomp/power/proc.ml
+++ asmcomp/power/proc.ml
@@ -116,14 +116,33 @@ let loc_float last_float make_stack reg_use_stack int float ofs =
   if !float <= last_float then begin
     let l = phys_reg !float in
     incr float;
-    (* Passing a float in a float register reserves a normal register as well *)
-    incr int;
+    (* On 64-bit platforms, passing a float in a float register
+       reserves a normal register as well *)
+    if size_int = 8 then incr int;
     if reg_use_stack then ofs := !ofs + size_float;
     l
   end else begin
     ofs := Misc.align !ofs size_float;
     let l = stack_slot (make_stack !ofs) Float in
     ofs := !ofs + size_float; l
+  end
+
+let loc_int_pair last_int make_stack int ofs =
+  (* 64-bit quantities split across two registers must either be in a
+     consecutive pair of registers where the lowest numbered is an
+     even-numbered register; or in a stack slot that is 8-byte aligned. *)
+  int := Misc.align !int 2;
+  if !int <= last_int - 1 then begin
+    let reg_lower = phys_reg !int in
+    let reg_upper = phys_reg (1 + !int) in
+    int := !int + 2;
+    [| reg_lower; reg_upper |]
+  end else begin
+    ofs := Misc.align !ofs 8;
+    let stack_lower = stack_slot (make_stack !ofs) Int in
+    let stack_upper = stack_slot (make_stack (size_int + !ofs)) Int in
+    ofs := !ofs + 8;
+    [| stack_lower; stack_upper |]
   end
 
 let calling_conventions first_int last_int first_float last_float
@@ -165,7 +184,25 @@ let loc_results res =
   let (loc, _ofs) = calling_conventions 0 15 101 113 not_supported 0 res
   in loc
 
-(* C calling conventions for ELF64v2:
+(* C calling conventions for ELF32:
+     use GPR 3-10 and FPR 1-8 just like ML calling conventions.
+     Using a float register does not affect the int registers.
+     Always reserve 8 bytes at bottom of stack (linkage area: back chain
+     + LR save word).  The 8 reserved bytes are automatically added in
+     emit.mlp (reserved_stack_space) and need not appear here.
+     For non-variadic functions, overflow arguments start immediately
+     after the linkage area (no mandatory parameter save area).
+   C calling conventions for ELF64v1:
+     Use GPR 3-10 for the first integer arguments.
+     Use FPR 1-13 for the first float arguments.
+     Always reserve stack space for all arguments, even when passed in
+     registers.
+     Always reserve at least 8 words (64 bytes) for the arguments.
+     Always reserve 48 bytes at bottom of stack, plus whatever is needed
+     to hold the arguments.
+     The reserved 48 bytes are automatically added in emit.mlp
+     and need not appear here.
+   C calling conventions for ELF64v2:
      Use GPR 3-10 for the first integer arguments.
      Use FPR 1-13 for the first float arguments.
      If all arguments fit in registers, don't reserve stack space.
@@ -186,9 +223,16 @@ let external_calling_conventions
   List.iteri
     (fun i ty_arg ->
       match ty_arg with
-      | XInt | XInt32 | XInt64 ->
+      | XInt | XInt32 ->
         loc.(i) <-
           [| loc_int last_int make_stack reg_use_stack int ofs |]
+      | XInt64 ->
+          if size_int = 4 then begin
+            assert (not reg_use_stack);
+            loc.(i) <- loc_int_pair last_int make_stack int ofs
+          end else
+            loc.(i) <-
+              [| loc_int last_int make_stack reg_use_stack int ofs |]
       | XFloat ->
         loc.(i) <-
           [| loc_float last_float make_stack reg_use_stack int float ofs |])
@@ -196,15 +240,25 @@ let external_calling_conventions
   (loc, Misc.align !ofs 16) (* Keep stack 16-aligned *)
 
 let loc_external_arguments ty_args =
-  let (loc, ofs) =
-    external_calling_conventions 0 7 101 113 outgoing 0 true ty_args in
-  if Array.exists
-       (fun r ->
-          assert (Array.length r = 1);
-          match r.(0).loc with Stack _ -> true | _ -> false)
-       loc
-  then (loc, ofs)
-  else (loc, 0)
+  match abi with
+  | ELF32 ->
+      external_calling_conventions 0 7 101 108 outgoing 0 false ty_args
+  | ELF64v1 ->
+      let (loc, ofs) =
+        external_calling_conventions 0 7 101 113 outgoing 0 true ty_args in
+      (loc, max ofs 64)
+  | ELF64v2 ->
+      let (loc, ofs) =
+        external_calling_conventions 0 7 101 113 outgoing 0 true ty_args in
+      if Array.fold_left
+           (fun stk r ->
+              assert (Array.length r = 1);
+              match r.(0).loc with
+              | Stack _ -> true
+              | _ -> stk)
+           false loc
+      then (loc, ofs)
+      else (loc, 0)
 
 (* Results are in GPR 3 and FPR 1 *)
 
@@ -216,10 +270,20 @@ let loc_external_results res =
 
 let loc_exn_bucket = phys_reg 0
 
-(* For ELF64v2 see:
+(* For ELF32 see:
+   "System V Application Binary Interface PowerPC Processor Supplement"
+   http://refspecs.linux-foundation.org/elf/elfspec_ppc.pdf
+
+   For ELF64v1 see:
+   "64-bit PowerPC ELF Application Binary Interface Supplement 1.9"
+   http://refspecs.linuxfoundation.org/ELF/ppc64/PPC-elf64abi.html
+
+   For ELF64v2 see:
    "64-Bit ELF V2 ABI Specification -- Power Architecture"
    http://openpowerfoundation.org/wp-content/uploads/resources/leabi/
      content/dbdoclet.50655239___RefHeading___Toc377640569.html
+
+   All of these specifications seem to agree on the numberings we need.
 *)
 
 let int_dwarf_reg_numbers =
@@ -251,11 +315,22 @@ let regs_are_volatile _rs = false
 
 (* Registers destroyed by operations *)
 
-(* For direct C calls, all caller-save registers are destroyed,
-   plus GPR28 because it is used to save the OCaml stack pointer. *)
 let destroyed_at_c_call =
+  (* On ELF32, non-allocating C calls always go through caml_c_call_stack_args
+     (stack_ofs >= 16 due to parameter save area).  The emitter clobbers:
+     - r25 (phys 19): C function address loaded via emit_symbol_load_got
+     - r24 (phys 18): stack arg size loaded via li
+     - r27 (phys 21): return address saved via mflr in caml_c_call_stack_args
+     - r28 (phys 22): OCaml SP saved via mr in caml_c_call_stack_args
+     On ELF64, stack_ofs can be 0 and uses the inline path (which does not
+     clobber r27), but stack_ofs > 0 still goes through caml_c_call_stack_args.
+     Being conservative: mark all four for all ABIs. *)
   Array.of_list(List.map phys_reg
-    [0; 1; 2; 3; 4; 5; 6; 7; 21;  (* 21 = r28, C_CALL_TMP *)
+    (* Phys reg numbering (post ALLOC_PTR-to-r23 move):
+         17=r24, 18=r25, 20=r27, 21=r28 (C_CALL_TMP).
+       r24, r25, r27 are only clobbered on ELF32 by caml_c_call_stack_args,
+       but marking them everywhere is a safe over-approximation. *)
+    [0; 1; 2; 3; 4; 5; 6; 7; 17; 18; 20; 21;
      100; 101; 102; 103; 104; 105; 106; 107; 108; 109; 110; 111; 112; 113])
 
 let destroyed_at_oper = function
@@ -265,6 +340,10 @@ let destroyed_at_oper = function
       destroyed_at_c_call
   | Iop(Iintoffloat | Istore(Single, _, _)) ->
       [| phys_reg 100 |] (* FPR0 destroyed *)
+  | Iop(Ifloatofint) when size_int = 4 ->
+      [| phys_reg 100; phys_reg 101 |] (* FPR0 and FPR1 destroyed *)
+  | Iop(Ifloatofint) ->
+      [| phys_reg 100 |] (* FPR0 destroyed (PPC64 doesn't use FPR0 here) *)
   | _ -> [||]
 
 let destroyed_at_raise = all_phys_regs
@@ -282,6 +361,8 @@ let max_register_pressure =
   function
     Iextcall _ -> [| 13; 18 |]
   | Iintoffloat | Istore(Single, _, _) -> [| n; 31 |]
+  | Ifloatofint when size_int = 4 -> [| n; 30 |]
+  | Ifloatofint -> [| n; 31 |]
   | _ -> [| n; 32 |]
 
 (* Calling the assembler *)
